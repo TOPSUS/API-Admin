10d26da1ffeadc07c68ab86effdb73377c1edd1a
Wahyu <wahyuivan999@ymail.com>
2021-05-04 15:09:48 +0800

    p

--- app/Http/Controllers/BeritaPelabuhanController.php
+++ app/Http/Controllers/BeritaPelabuhanController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\BeritaPelabuhan;
+use Auth;
 class BeritaPelabuhanController extends Controller
 {
     /**
@@ -14,6 +15,34 @@ class BeritaPelabuhanController extends Controller
     public function index()
     {
         //
+        $data = array();
+        $id_user = Auth::user()->id;
+        $listBerita = BeritaPelabuhan::where('id_user', $id_user)->get();
+
+        // dd(count($listKapal));
+
+        $temp = array();
+        foreach ($listBerita as $list) {
+            $berita = BeritaPelabuhan::find($list->id);
+            if (isset($berita)) {
+                array_push($temp, [
+                    'id' => $berita->id,
+                    'judul' => $berita->judul,
+                    'foto' => $berita->foto,
+                    'created' => $berita->created_at,
+                ]);
+            }
+        }
+
+        $data = [
+            'list_berita' => $temp
+        ];
+
+        return response([
+            'status' => 200,
+            'data' => $data,
+            'message' => 'list data berita fetched'
+        ]);
     }
 
     /**
